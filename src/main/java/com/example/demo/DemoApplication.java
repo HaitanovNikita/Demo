48b6cbba0fbf9754d108e0a1fc591c686546feb8
nhaitanov <nhaitanov@integrity.com.ua>
2020-10-20 17:49:24 +0300

    add anotations

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,13 @@ import com.example.demo.controller.DemoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan("com.example.demo.*")
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 @RequiredArgsConstructor
 //@ConfigurationProperties("api")
+@Profile("test")
 public class DemoController {
     @Value("${api.url}")
     private String url;
